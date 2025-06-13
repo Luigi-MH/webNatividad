fbf72d5eb7362945957c674418fe8b99a4f99cc2
Luigi-MH <mendozaherreraluigi@gmail.com>
2025-06-12 20:53:16 -0500

    AVANCE 1

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ClientsComponent } from "../../components/clients/clients.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ClientsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
